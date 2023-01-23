7c88ebfd5e37d414d228768d2ed6df03d809ac70
Eros Yael Castañeda Téllez <122810314+ErosCasta@users.noreply.github.com>
2023-01-23 17:01:19 +0000

    semana2

---
+++ semana2/programa1.py
@@ -0,0 +1,9 @@
+"""     
+        Programa1
+        Nombre: Eros YCT
+        Fecha: 23/01/2023
+        Descripcion: comentarios multilinea, comentarios de una linea, comentarios de concatenacion de cadena 
+"""
+variable1 = "Hola"  # Variable que almacena una cadena de caracteres
+variable2 = "Python"  # Variable que almacena una cadena de caracteres 
+print(variable1 + variable2) #concatena 2 cadenas 
