6258507714cf53b2af25d8b49bb095d459ce7110
Eros Yael Castañeda Téllez <122810314+ErosCasta@users.noreply.github.com>
2023-01-24 19:57:34 +0000

    creamos el programa2.py

---
+++ semana2/programa2.py
@@ -0,0 +1,9 @@
+"""     
+        Programa2
+        Nombre: Eros YCT
+        Fecha: 24/01/2023
+        Descripcion: 
+"""
+variable1 = "hola" # variable tipo
+variable2 = 619 # variable tipo
+print ("{} {}" .format (variable1 , variable2))
