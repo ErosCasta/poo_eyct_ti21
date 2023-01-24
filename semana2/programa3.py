79ec52b2eddea1816a4822ef358d72aa4e1d2ea9
Eros Yael Castañeda Téllez <122810314+ErosCasta@users.noreply.github.com>
2023-01-24 20:52:57 +0000

    operaciones

---
+++ semana2/programa3.py
@@ -0,0 +1,16 @@
+"""     
+        Programa3
+        Nombre: Eros YCT
+        Fecha: 24/01/2023
+        Descripcion: utilizaremos operacionesaritmrticas
+"""
+numero1 = 10
+numero2 = 5
+print(numero1 + numero2)
+print("{} + {}" .format(numero1,numero2 ))
+print("{} + {} = {}".format( numero1 , numero2 , (numero1 + numero2 )))
+print("{} = {} * {}".format((numero1 * numero2), numero1, numero2))
+numero3 = 12
+numero4 = 3
+print("el resultado de dividir {} / {} = " .format(numero3 , numero4))
+print(numero3 / numero4)
