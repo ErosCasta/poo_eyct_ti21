b0f5fda2683193f00aa04106a0d6f0a034fdc2dc
Eros Yael Castañeda Téllez <122810314+ErosCasta@users.noreply.github.com>
2023-01-26 19:50:51 +0000

    acceso a las variables por posición y nombre

---
+++ semana2/programa4.py
@@ -0,0 +1,12 @@
+"""     
+        Programa4
+        Nombre: Eros YCT
+        Fecha: 26/01/2023
+        Descripcion: acceso a las variables por posición y nombre
+"""
+numero1 = 10
+numero2 = 5
+print("{} + {} = {} ".format(numero1 , numero2, numero1 + numero2))
+print("{n1} + {n2} = {suma} ".format(n1=numero1, n2=numero2, suma= numero1 + numero2))
+print("{n2} + {n2} = {n2} ".format(n1=numero1, n2=numero2, suma= numero1 + numero2))
+print("{numero1} + {numero2} = {suma} ".format(numero1=numero1, numero2=numero2, suma= numero1 + numero2))
