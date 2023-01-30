1b5407231ad21bac8fb6c1bdc503a5d732aec389
Eros Yael Castañeda Téllez <122810314+ErosCasta@users.noreply.github.com>
2023-01-30 16:56:11 +0000

    se creó el comando para sumar, restar, dividir y potencia

---
+++ semana3/programa5.py
@@ -0,0 +1,26 @@
+"""     
+        Programa5
+        Nombre: Eros YCT
+        Fecha: 30/01/2023
+        Descripcion:
+"""
+numero1 = input("Número 1:")
+numero2 = 10
+suma = int(numero1) + numero2
+print(suma)
+numero1 = input("Número 1:")
+numero2 = 10
+resta = int(numero1) - numero2
+print(resta)
+numero1 = input("Número 1:")
+numero2 = 10
+multiplicacion = int(numero1) * numero2
+print(multiplicacion)
+numero1 = input("Número 1:")
+numero2 = 10
+división = int(numero1) / numero2
+print(división)
+numero1 = input("Número 1:")
+numero2 = 10
+potencia = int(numero1) ** numero2
+print(potencia)
