e299713ba0fa496ec4ca4b4143869b818e4649aa
Eros Yael Castañeda Téllez <122810314+ErosCasta@users.noreply.github.com>
2023-02-02 00:35:19 +0000

    Corrección de programa6

--- semana3/programa6.py
+++ semana3/programa6.py
@@ -8,28 +8,17 @@ print("Comencemos por el área del triangulo")
 base = None
 altura = None
 
-while True:
-  try:
-    base = float(input("Escriba la base del triangulo: "))
-    break
-  except:
-    print("Debe ser un número")
+base = float(input("Escriba la base del triangulo: ")) #variable de tipo int o float
 
-while True:
-  try:
-    altura = float(input("Escriba la altura del triangulo: "))
-    break
-  except:
-    print("Debe ser un número")
-
+altura = float(input("Escriba la altura del triangulo: ")) #variable de tipo int o float
 area = base * altura / 2
 
-print("El area del triángulo es: {}".format(area))
+print("El area del triángulo es: {}".format(area)) #variable de tipo int o float
 
 print("Ahora pasemos al perímetro del triangulo")
 
-lado1 = float(input("lado1: "))
-lado2 = float(input("lado2: "))
-lado3 = float(input("lado3: "))
+lado1 = float(input("lado1: ")) #variable de tipo int o float
+lado2 = float(input("lado2: ")) #variable de tipo int o float
+lado3 = float(input("lado3: ")) #variable de tipo int o float
 perimetro = lado1 + lado2 + lado3 
-print("El perimetro del triangulo es: {} cm".format(perimetro))
+print("El perimetro del triangulo es: {} cm".format(perimetro)) #variable de tipo int o float
