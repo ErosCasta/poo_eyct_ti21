2f6f8d214175f2c39b52a849389f206af9815253
Eros Yael Castañeda Téllez <122810314+ErosCasta@users.noreply.github.com>
2023-02-09 01:36:51 +0000

    Se practicó la manera de sacar área y perímetro de un circulo y cuadrado

---
+++ reto/programa15.py
@@ -0,0 +1,28 @@
+"""     
+        Programa15
+        Nombre: Eros YCT
+        Fecha: 08/02/2023
+        Descripcion: 
+"""
+print("comencemos con el area y perimetro del circulo")
+print("Ingresa el radio del circulo")
+r = input("radio: ") #variable de tipo int o float
+area = 3.14 * int(r) * int(r)
+print("El area de un circulo de",r,"de radio es",area) #variable de tipo int o float
+print("ahora pasemos al perimetro")
+print("ingrese el radio del circulo")
+r = input("radio: ") #variable de tipo int o float
+perimetro = 3.14 * int(r)**2
+print("El perimetro de un círculo de",r,"de radio es",perimetro) #variable de tipo int o float
+print()
+print("Pasemos al área y perimetro del cuadrado")
+print("Comencemos por el área")
+print("ingresa la medida de su lado")
+l = input("lado: ") #variable de tipo int o float
+area = int(l) * int(l)
+print("el area del cuadrado es de: ",perimetro) #variable de tipo int o float
+print("pasemos al perimetro")
+print("ingresa medida del lado")
+l = input("lado: ") #variable de tipo int o float
+perimetro = int(l) + int(l) + int(l) + int(l)
+print("el perimetro del cuadrado es: ",perimetro) #variable de tipo int o float
